6ac31405d8784164f5c616b0d5b567b9b9ce6d45
Tim <timlaroc95@gmail.com>
2019-08-28 00:06:05 -0400

    Baseline Code for Characters
    Trying to get some abstraction working by passing variables through send. Might have to do some digging.

---
+++ Messages/newCharacterPrompts.js
@@ -0,0 +1,12 @@
+class NewCharacterPrompts {
+    // Greeting
+    NewCharacterPrompts() {
+        var Greeting = new string();
+        this.Greeting = 'Welcome to the West Marches, a D&D 5e game! I will be helping you through your character creation process.';
+    }
+    // Choose Class
+    // Choose Race
+    // Roll stats
+}
+
+module.exports = NewCharacterPrompts;
